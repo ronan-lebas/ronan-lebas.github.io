19bffc93d0595464cdd9315ad6398e4d4bd4ff36
ronan-lebas <80069231+ronan-lebas@users.noreply.github.com>
2024-05-23 16:32:00 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -9,7 +9,7 @@ const CONFIG = {
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/gitprofile/',
+  base: '/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
@@ -25,30 +25,30 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: [], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
-    external: {
-      //header: 'My Projects',
-      // To hide the `External Projects` section, keep it empty.
-      //projects: [
-      //  {
-      //    title: 'Project Name',
-      //    description:
-      //      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      //    imageUrl:
-      //      'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-      //    link: 'https://example.com',
-      //  },
-      //  {
-      //    title: 'Project Name',
-      //    description:
-      //      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      //    imageUrl:
-      //      'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-       //   link: 'https://example.com',
-      //  },
-      //],
+    //external: {
+    //  header: 'My Projects',
+    //  // To hide the `External Projects` section, keep it empty.
+    //  projects: [
+    //    {
+    //      title: 'Project Name',
+    //      description:
+    //        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+    //      imageUrl:
+    //        'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+    //      link: 'https://example.com',
+    //    },
+    //    {
+    //      title: 'Project Name',
+    //      description:
+    //        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+    //      imageUrl:
+    //        'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+    //      link: 'https://example.com',
+    //    },
+    //  ],
     //},
   },
   seo: {
@@ -83,60 +83,82 @@ const CONFIG = {
       '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    
+    ''
   ],
   experiences: [
-   // {
-   //   company: 'Company Name',
-   //   position: 'Position',
-   //   from: 'September 2021',
-   //   to: 'Present',
-   //   companyLink: 'https://example.com',
-   // },
+    //{
+    //  company: 'Company Name',
+    //  position: 'Position',
+    //  from: 'September 2021',
+    //  to: 'Present',
+    //  companyLink: 'https://example.com',
+    //},
+    //{
+    //  company: 'Company Name',
+    //  position: 'Position',
+    //  from: 'July 2019',
+    //  to: 'August 2021',
+    //  companyLink: 'https://example.com',
+    //},
   ],
   certifications: [
-   // {
-     // name: 'Lorem ipsum',
-      //body: 'Lorem ipsum dolor sit amet',
-      //year: 'March 2022',
-     // link: 'https://example.com',
+    //{
+    //  name: 'Lorem ipsum',
+    //  body: 'Lorem ipsum dolor sit amet',
+    //  year: 'March 2022',
+    //  link: 'https://example.com',
     //},
   ],
   educations: [
     //{
     //  institution: 'Institution Name',
     //  degree: 'Degree',
+    //  from: '2015',
+    //  to: '2019',
+    //},
+    //{
+    //  institution: 'Institution Name',
+    //  degree: 'Degree',
     //  from: '2012',
     //  to: '2014',
-   // }
+    //},
   ],
   publications: [
-//    {
-//      title: 'Publication Title',
-//      conferenceName: '',
-//      journalName: 'Journal Name',
-//      authors: 'John Doe, Jane Smith',
-//      link: 'https://example.com',
-//      description:
-//        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-//    }
+    //{
+    //  title: 'Publication Title',
+    //  conferenceName: '',
+    //  journalName: 'Journal Name',
+    //  authors: 'John Doe, Jane Smith',
+    //  link: 'https://example.com',
+    //  description:
+    //    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+    //},
+    //{
+    //  title: 'Publication Title',
+    //  conferenceName: 'Conference Name',
+    //  journalName: '',
+    //  authors: 'John Doe, Jane Smith',
+    //  link: 'https://example.com',
+    //  description:
+    //    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+    //},
   ],
   // Display articles from your medium or dev account. (Optional)
   //blog: {
   //  source: 'dev', // medium | dev
   //  username: 'arifszn', // to hide blog section, keep it empty
-  //  limit: 2, // How many articles to display. Max is 10.
+  //  limit: 3, // How many articles to display. Max is 10.
   //},
   //googleAnalytics: {
   //  id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   //},
-  // Track visitor interaction and behavior. https://www.hotjar.com
+  //// Track visitor interaction and behavior. https://www.hotjar.com
   //hotjar: {
   //  id: '',
   //  snippetVersion: 6,
   //},
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'nord',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -206,6 +228,6 @@ const CONFIG = {
     >GitProfile</a> and ❤️`,
 
   enablePWA: true,
-}}
+};
 
 export default CONFIG;
